b744bbfc167032d1b894802ca752e0db88569bfc
richolsu <38938914+richolsu@users.noreply.github.com>
2018-09-06 05:01:24 +0700

    Home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,53 +1,304 @@
 import React from 'react';
-import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, FlatList, Image, Text, View, Dimensions } from 'react-native';
+import { AppStyles, ButtonStyle, TextStyle, TextInputStyle } from '../AppStyles'
+import Button from 'react-native-button';
 import Hamburger from '../components/Hamburger';
+import Carousel, { Pagination } from 'react-native-snap-carousel';
 
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get('window');
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text> Home Screen </Text>
-    <Image source={require('../../assets/icons/menu.png')} />
-    <Button
-      title="Go to Food"
-      onPress={() => navigation.openDrawer()}
-    />
-    <Button
-      title="Go to Categories"
-      onPress={() => navigation.navigate('CategoryList')}
-    />
-    <Button
-      title="Go to Search"
-      onPress={() => navigation.navigate('Search')}
-    />
-    <Button
-      title="Go to Cart2"
-      onPress={() => navigation.dispatch({ type: 'Cart' })}
-    />
-    <Button
-      title="Go to Reservation"
-      onPress={() => navigation.navigate('Reservation')}
-    />
-    <Button
-      title="Go to Order"
-      onPress={() => navigation.navigate('OrderList')}
-    />
-    <Button
-      title="Go to Welcome"
-      onPress={() => navigation.navigate('Welcome')}
-    />
-  </View>
-);
+export default class HomeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: <Hamburger onPress={() => { navigation.openDrawer() }} />,
+  });
 
-HomeScreen.navigationOptions = ({ navigation }) => ({
-  headerLeft: <Hamburger onPress={()=>{navigation.openDrawer()}}/>
-});
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeSlide: 1,
+      data: { deals: [] },
+      loading: false,
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  json = require('../jsons/home.json');
+
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          // data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          data: this.json.results,
+          error: res.error || null,
+          loading: false,
+          refreshing: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error, loading: false });
+      });
+  };
+
+  renderCategoryItem = ({ item }) => (
+    <View style={styles.categoryItemContainer}>
+      <Image style={styles.categoryItemPhoto} source={{ uri: item.photo }} />
+      <Text style={styles.categoryItemTitle}>{item.name}</Text>
+    </View>
+  );
+  renderFoodItem = ({ item }) => (
+    <View style={styles.foodItemContainer}>
+      <Image style={styles.foodPhoto} source={{ uri: item.photo }} />
+      <View style={styles.foodInfo}>
+        <Text style={styles.foodName}>{item.name}</Text>
+        <Text style={styles.foodPrice}>${item.price}</Text>
+      </View>
+    </View>
+  );
+
+  renderDealItem({ item, index }) {
+    return (
+      <View style={styles.dealItemContainer}>
+        <Image style={StyleSheet.absoluteFillObject} source={{ uri: item.photo }} />
+        <View style={styles.overlay} />
+        <Text style={styles.dealName}>{item.name}</Text>
+      </View>
+    );
+  }
+  render() {
+    const { activeSlide } = this.state;
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}> Popular Categories </Text>
+        <View style={styles.categories}>
+          <FlatList
+            horizontal={true}
+            data={this.state.data.categories}
+            renderItem={this.renderCategoryItem}
+            keyExtractor={item => `${item.id}`}
+          />
+        </View>
+        <Text style={styles.title}> Best Deals </Text>
+        <View style={styles.deals}>
+          <Carousel
+            ref={(c) => { this._slider1Ref = c; }}
+            data={this.state.data.deals}
+            renderItem={this.renderDealItem}
+            sliderWidth={viewportWidth}
+            itemWidth={viewportWidth}
+            hasParallaxImages={true}
+            inactiveSlideScale={1}
+            inactiveSlideOpacity={1}
+            firstItem={1}
+            loop={true}
+            // loopClonesPerSide={2}
+            autoplay={true}
+            autoplayDelay={500}
+            autoplayInterval={3000}
+            onSnapToItem={(index) => this.setState({ activeSlide: index })}
+          />
+          <Pagination
+            dotsLength={this.state.data.deals.length}
+            activeDotIndex={activeSlide}
+            containerStyle={styles.paginationContainer}
+            dotColor={'rgba(255, 255, 255, 0.92)'}
+            dotStyle={styles.paginationDot}
+            inactiveDotColor='white'
+            inactiveDotOpacity={0.4}
+            inactiveDotScale={0.6}
+            carouselRef={this._slider1Ref}
+            tappableDots={!!this._slider1Ref}
+          />
+        </View>
+        <Text style={styles.title}> Most Popular </Text>
+        <View style={styles.foods}>
+          <FlatList
+            data={this.state.data.foods}
+            renderItem={this.renderFoodItem}
+            keyExtractor={item => `${item.id}`}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
 
 
 const styles = StyleSheet.create({
-  icon: {
-    width: 24,
-    height: 24,
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
   },
-});
+  categories: {
+    marginTop: 10,
+  },
+  categoryItemContainer: {
+    margin: 10,
+    alignItems: 'center'
+  },
+  categoryItemPhoto: {
+    height: 100,
+    width: 100,
+    borderRadius: 50
+  },
+  categoryItemTitle: {
+    color: 'black',
+  },
+  deals: {
+    marginTop: 10,
+  },
+  dealItemContainer: {
+    flex: 1,
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    width: viewportWidth,
+    height: 200,
+    backgroundColor: 'green'
+  },
+  dealPhoto: {
+    width: '100%',
+    height: 200,
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  dealName: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+  paginationContainer: {
+    flex: 1,
+    position: 'absolute',
+    alignSelf: 'center',
+    // backgroundColor: 'green',
+    paddingVertical: 8,
+    marginTop:150,
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 0
+  },
 
-export default HomeScreen;
+
+  foods: {
+    marginTop: 10,
+  },
+  foodItemContainer: {
+    flex: 1,
+    margin: 10,
+    marginBottom: 20,
+  },
+  foodPhoto: {
+    width: '100%',
+    height: 200,
+  },
+  foodInfo: {
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  foodName: {
+    flex: 1,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    color: 'black',
+  },
+  foodPrice: {
+    flex: 1,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    color: 'black',
+  },
+  title: {
+    color: AppStyles.color.text,
+    fontSize: 25,
+  },
+  photo: {
+    height: 300,
+  },
+  detail: {
+    height: 60,
+    width: 100,
+    marginRight: 10
+  },
+
+  description: {
+    marginTop: 20,
+    color: AppStyles.color.text,
+  },
+  buttonSetContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSet: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderRadius: AppStyles.borderRadius.main,
+    borderColor: AppStyles.color.grey
+  },
+  count: {
+    padding: 10,
+    marginTop: 2,
+    color: AppStyles.color.text,
+    fontFamily: AppStyles.fontName.bold,
+    fontWeight: 'bold',
+    textAlign: "center",
+  },
+  buttonContainer: {
+    padding: 10,
+    width: 50,
+  },
+  buttonText: {
+    color: AppStyles.color.text
+  },
+  price: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    borderWidth: 1,
+    fontWeight: 'bold',
+    padding: 10,
+    textAlign: 'center',
+    color: AppStyles.color.text,
+    borderColor: AppStyles.color.grey,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 50,
+  },
+  actionButtonContainer: {
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginLeft: 10,
+    backgroundColor: AppStyles.color.main
+  },
+  actionButtonText: {
+    color: AppStyles.color.white
+  },
+
+
+
+});

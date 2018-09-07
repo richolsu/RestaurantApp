4bb6309bad6adf7129ff09c60ca8d52f573639ee
richolsu <38938914+richolsu@users.noreply.github.com>
2018-09-08 04:09:48 +0700

    fix ui

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -42,7 +42,7 @@ import { connect } from 'react-redux';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',

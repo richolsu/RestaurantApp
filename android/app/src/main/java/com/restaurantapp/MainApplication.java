a411d706e953964bc62a49e77be41cf5b105b876
richolsu <38938914+richolsu@users.noreply.github.com>
2018-09-12 01:44:43 +0700

    firebase

--- android/app/src/main/java/com/restaurantapp/MainApplication.java
+++ android/app/src/main/java/com/restaurantapp/MainApplication.java
@@ -3,11 +3,15 @@ package com.restaurantapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.storage.RNFirebaseStoragePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +27,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
+        new MainReactPackage(),
+        new RNFirebasePackage(),
+        new VectorIconsPackage(),
+        new RNFirebaseStoragePackage(),
+        new RNFirebaseAuthPackage()
       );
     }
 
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { Component } from "react";
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Button from 'react-native-button';
 import { AppStyles, ButtonStyle, TextInputStyle, TextStyle } from '../AppStyles';
+import firebase from 'react-native-firebase';
 
-const LoginScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={[TextStyle.title, TextStyle.leftTitle]}>Sign In</Text>
-    <View style={TextInputStyle.container}>
-      <TextInput style={TextInputStyle.body} placeholder="E-mail or phone number" placeholderTextColor={AppStyles.color.grey} underlineColorAndroid='transparent' />
-    </View>
-    <View style={TextInputStyle.container}>
-      <TextInput style={TextInputStyle.body} secureTextEntry={true} placeholder="Password" placeholderTextColor={AppStyles.color.grey} underlineColorAndroid='transparent' />
-    </View>
-    <Button containerStyle={ButtonStyle.loginContainer} style={ButtonStyle.loginText}
-      onPress={() => navigation.dispatch({ type: 'Login' })}>Log in</Button>
-    <Text style={styles.or}>OR</Text>
-    <Button containerStyle={ButtonStyle.facebookContainer} style={ButtonStyle.facebookText}
-      onPress={() => navigation.dispatch({ type: 'Login' })}>Facebook Login</Button>
-  </View>
-);
+class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        loading: false,
+        user,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authSubscription();
+  }
+
+  onPressLogin = () => {
+    this.props.navigation.dispatch({ type: 'Login' });
+  }
+
+  onPressFacebook = () => {
+    this.props.navigation.dispatch({ type: 'Login' });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={[TextStyle.title, TextStyle.leftTitle]}>Sign In</Text>
+        <View style={TextInputStyle.container}>
+          <TextInput style={TextInputStyle.body} placeholder="E-mail or phone number" placeholderTextColor={AppStyles.color.grey} underlineColorAndroid='transparent' />
+        </View>
+        <View style={TextInputStyle.container}>
+          <TextInput style={TextInputStyle.body} secureTextEntry={true} placeholder="Password" placeholderTextColor={AppStyles.color.grey} underlineColorAndroid='transparent' />
+        </View>
+        <Button containerStyle={ButtonStyle.loginContainer} style={ButtonStyle.loginText}
+          onPress={() => this.onPressLogin()}>Log in</Button>
+        <Text style={styles.or}>OR</Text>
+        <Button containerStyle={ButtonStyle.facebookContainer} style={ButtonStyle.facebookText}
+          onPress={() => this.onPressFacebook()}>Facebook Login</Button>
+      </View>
+    );
+  }
+}
+
 
 
 const styles = StyleSheet.create({
